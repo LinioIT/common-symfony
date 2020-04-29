995d0e0d6af241f10b76397009458258d2165b0e
Tamara Dietz <melinka@hotmail.com>
2020-04-29 13:29:19 -0400

    refactor: namespace for the templating bundle

--- src/Controller/TemplatingAware.php
+++ src/Controller/TemplatingAware.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Linio\Common\Symfony\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
